10deb373d2124fef45ef67841354508a6eed56c7
MDalamin5 <mdal.amin5@northsouth.edu>
2025-03-05 12:40:40 +0600

    update this code

--- app.py
+++ app.py
@@ -41,14 +41,17 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     st.session_state.messages.append({"role":"user","content":prompt})
     st.chat_message("user").write(prompt)
 
-    llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
-    tools=[search,arxiv,wiki]
+    if not api_key:
+        st.warning("Please enter your Groq API key in the sidebar to use this chatbot. You can get a free API key at [groq.com](https://console.groq.com/playground).")
+    else:
 
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+        llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+        tools=[search,arxiv,wiki]
 
-    with st.chat_message("assistant"):
-        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
-        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
-        st.session_state.messages.append({'role':'assistant',"content":response})
-        st.write(response)
+        search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
 
+        with st.chat_message("assistant"):
+            st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+            response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
+            st.session_state.messages.append({'role':'assistant',"content":response})
+            st.write(response)
